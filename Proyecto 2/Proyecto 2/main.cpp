13b3e5fa52f0c5d6e3d1a4235b21af577b540a82
luiscarlo6 <luiscarlo6@gmail.com>
2015-01-11 16:52:39 -0430

    Inicializado Proyecto 2

---
+++ Proyecto 2/Proyecto 2/main.cpp
@@ -0,0 +1,70 @@
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <iostream>
+
+using namespace std;
+
+#define DEF_floorGridScale	0.1f
+#define DEF_floorGridXSteps	10.0f
+#define DEF_floorGridZSteps	10.0f
+
+
+void changeViewport(int w, int h) {
+	glViewport(0,0,w,h);
+}
+
+void render(){
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	GLfloat zExtent, xExtent, xLocal, zLocal;
+    int loopX, loopZ;
+
+	/* Render Grid */
+	glPushMatrix();
+    glColor3f( 0.0f, 0.7f, 0.7f );
+    glBegin( GL_LINES );
+    zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
+    for(loopX = -DEF_floorGridXSteps; loopX <= DEF_floorGridXSteps; loopX++ )
+	{
+	xLocal = DEF_floorGridScale * loopX;
+	glVertex3f( xLocal, -zExtent, 0.0f );
+	glVertex3f( xLocal, zExtent,  0.0f );
+	}
+    xExtent = DEF_floorGridScale * DEF_floorGridXSteps;
+    for(loopZ = -DEF_floorGridZSteps; loopZ <= DEF_floorGridZSteps; loopZ++ )
+	{
+	zLocal = DEF_floorGridScale * loopZ;
+	glVertex3f( -xExtent, zLocal, 0.0f );
+	glVertex3f(  xExtent, zLocal, 0.0f );
+	}
+    glEnd();
+    glPopMatrix();
+
+
+	glutSwapBuffers();
+}
+
+int main (int argc, char** argv) {
+
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+
+	glutInitWindowSize(800,600);
+
+	glutCreateWindow("Test Opengl");
+
+	glutReshapeFunc(changeViewport);
+	glutDisplayFunc(render);
+
+	
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW error");
+		return 1;
+	}
+	
+	glutMainLoop();
+	return 0;
+
+}
