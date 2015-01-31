6b2738ba265c0de6382a66cdc987440b4ff128e8
luiscarlo6 <luiscarlo6@gmail.com>
2015-01-30 21:21:41 -0430

    ya dibuja el plano sin nada, pero no se cuantos knots usar

--- Proyecto 3/Proyecto 3/mainNurbsProyecto_base01.cpp
+++ Proyecto 3/Proyecto 3/mainNurbsProyecto_base01.cpp
@@ -11,52 +11,103 @@ using namespace std;
 #define DEF_floorGridZSteps	10.0
 
 
-GLUnurbsObj *theNurb;
-
 void ejesCoordenada() {
-	
+
 	glLineWidth(2.5);
 	glBegin(GL_LINES);
-		glColor3f(1.0,0.0,0.0);
-		glVertex2f(0,10);
-		glVertex2f(0,-10);
-		glColor3f(0.0,0.0,1.0);
-		glVertex2f(10,0);
-		glVertex2f(-10,0);
+	glColor3f(1.0,0.0,0.0);
+	glVertex2f(0,10);
+	glVertex2f(0,-10);
+	glColor3f(0.0,0.0,1.0);
+	glVertex2f(10,0);
+	glVertex2f(-10,0);
 	glEnd();
 
 	glLineWidth(1.5);
 	int i;
 	glColor3f(0.0,1.0,0.0);
 	glBegin(GL_LINES);
-		for(i = -10; i <=10; i++){
-			if (i!=0) {		
-				if ((i%2)==0){	
-					glVertex2f(i,0.4);
-					glVertex2f(i,-0.4);
+	for(i = -10; i <=10; i++){
+		if (i!=0) {		
+			if ((i%2)==0){	
+				glVertex2f(i,0.4);
+				glVertex2f(i,-0.4);
 
-					glVertex2f(0.4,i);
-					glVertex2f(-0.4,i);
-				}else{
-					glVertex2f(i,0.2);
-					glVertex2f(i,-0.2);
+				glVertex2f(0.4,i);
+				glVertex2f(-0.4,i);
+			}else{
+				glVertex2f(i,0.2);
+				glVertex2f(i,-0.2);
 
-					glVertex2f(0.2,i);
-					glVertex2f(-0.2,i);
+				glVertex2f(0.2,i);
+				glVertex2f(-0.2,i);
 
-				}
 			}
 		}
-		
+	}
+
 	glEnd();
 
 	glLineWidth(1.0);
 }
 
+void renderGrid()
+{
+	GLfloat zExtent, xExtent, xLocal, zLocal;
+	int loopX, loopZ;
+	// Render Grid 
+	glDisable(GL_LIGHTING);
+	glLineWidth(1.0);
+	glPushMatrix();
+	glRotatef(90,1.0,0.0,0.0);
+	glColor3f( 0.0, 0.7, 0.7 );
+	glBegin( GL_LINES );
+	zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
+	for(loopX = -DEF_floorGridXSteps; loopX <= DEF_floorGridXSteps; loopX++ )
+	{
+		xLocal = DEF_floorGridScale * loopX;
+		glVertex3f( xLocal, -zExtent, 0.0 );
+		glVertex3f( xLocal, zExtent,  0.0 );
+	}
+	xExtent = DEF_floorGridScale * DEF_floorGridXSteps;
+	for(loopZ = -DEF_floorGridZSteps; loopZ <= DEF_floorGridZSteps; loopZ++ )
+	{
+		zLocal = DEF_floorGridScale * loopZ;
+		glVertex3f( -xExtent, zLocal, 0.0 );
+		glVertex3f(  xExtent, zLocal, 0.0 );
+	}
+	glEnd();
+	ejesCoordenada();
+	glPopMatrix();
+	glEnable(GL_LIGHTING);
+	// Fin Grid
+}
+
+
+GLUnurbsObj *theNurb;
+
+GLfloat ctlpoints[21][21][3];
+
+float knots[50];
+
+void inicializarNots(){
+	int i;
+	float p = 1.0/18.0;
+	for(i = 0; i < 50; i++) {
+		if (i < 4) {
+			knots[i] = 0.0;
+		} else if (i > 20) {
+			knots[i] = 1.0;
+		} else {
+			knots[i] = p;
+			p = p + 1.0/18.0;
+		}
+	}
+}
+
+
 void changeViewport(int w, int h) {
 	
-	float aspectratio;
-
 	if (h==0)
 		h=1;
 
@@ -71,7 +122,18 @@ void changeViewport(int w, int h) {
 
 void init_surface() {
 	
-	
+	int f,c;
+	int z = 10;
+	for (f = 0; f < 21 ; f++) {
+		int x = 10;
+		for (c = 0; c < 21; c++) {
+			ctlpoints[f][c][0] = x;
+			ctlpoints[f][c][1] = 0.0;
+			ctlpoints[f][c][2] = z;
+			x--;
+		}
+		z--;
+	}
 	
 }
 
@@ -95,8 +157,6 @@ void init(){
 
 }
 
-
-
 void Keyboard(unsigned char key, int x, int y)
 {
   switch (key)
@@ -108,14 +168,29 @@ void Keyboard(unsigned char key, int x, int y)
   }
 }
 
+void controlPoints()
+{
+	int i,j;
+	glPointSize(5.0);
+	glDisable(GL_LIGHTING);
+	glColor3f(1.0, 1.0, 0.0);
+	glBegin(GL_POINTS);
+	for (i = 0; i <21; i++) {
+		for (j = 0; j < 21; j++) {
+			glVertex3f(ctlpoints[i][j][0], 	ctlpoints[i][j][1], ctlpoints[i][j][2]);
+		}
+	}
+	glEnd();
+	glEnable(GL_LIGHTING);
 
+
+}
 
 void render(){
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	GLfloat zExtent, xExtent, xLocal, zLocal;
-    int loopX, loopZ;
+	
 
 	glLoadIdentity ();                       
 	gluLookAt (25.0, 12.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
@@ -142,37 +217,8 @@ void render(){
 	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);   
 
-
+	//renderGrid();
 	
-	// Render Grid 
-	glDisable(GL_LIGHTING);
-	glLineWidth(1.0);
-	glPushMatrix();
-	glRotatef(90,1.0,0.0,0.0);
-    glColor3f( 0.0, 0.7, 0.7 );
-    glBegin( GL_LINES );
-    zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
-    for(loopX = -DEF_floorGridXSteps; loopX <= DEF_floorGridXSteps; loopX++ )
-	{
-	xLocal = DEF_floorGridScale * loopX;
-	glVertex3f( xLocal, -zExtent, 0.0 );
-	glVertex3f( xLocal, zExtent,  0.0 );
-	}
-    xExtent = DEF_floorGridScale * DEF_floorGridXSteps;
-    for(loopZ = -DEF_floorGridZSteps; loopZ <= DEF_floorGridZSteps; loopZ++ )
-	{
-	zLocal = DEF_floorGridScale * loopZ;
-	glVertex3f( -xExtent, zLocal, 0.0 );
-	glVertex3f(  xExtent, zLocal, 0.0 );
-	}
-    glEnd();
-	ejesCoordenada();
-    glPopMatrix();
-	glEnable(GL_LIGHTING);
-	// Fin Grid
-	
-
-
 	//Suaviza las lineas
 	glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable( GL_LINE_SMOOTH );	
@@ -182,10 +228,10 @@ void render(){
 
 	gluBeginSurface(theNurb);
     
-	/*gluNurbsSurface(theNurb, 
-                   25, variableKnots, 25, variableKnots,
-                   21 * 3, 3, variablePuntosControl, 
-                   4, 4, GL_MAP2_VERTEX_3);*/
+	gluNurbsSurface(theNurb, 
+                   25, knots, 25, knots,
+                   21 * 3, 3, &ctlpoints[0][0][0], 
+                   4, 4, GL_MAP2_VERTEX_3);
 	/*
 
 		No cambien los numeros de la funcion, solo deben de poner los nombres de las variables correspondiente.
@@ -199,21 +245,9 @@ void render(){
 	
 	
 	/* Muestra los puntos de control */
-	/*
-		int i,j;
-		glPointSize(5.0);
-		glDisable(GL_LIGHTING);
-		glColor3f(1.0, 1.0, 0.0);
-		glBegin(GL_POINTS);
-		for (i = 0; i <21; i++) {
-			for (j = 0; j < 21; j++) {
-	            glVertex3f(ctlpoints[i][j][0], 	ctlpoints[i][j][1], ctlpoints[i][j][2]);
-			}
-		}
-		glEnd();
-		glEnable(GL_LIGHTING);
-	*/
-		
+	
+	//controlPoints();
+
 
 	glDisable(GL_BLEND);
 	glDisable(GL_LINE_SMOOTH);
@@ -221,12 +255,15 @@ void render(){
 	glutSwapBuffers();
 }
 
+
 void animacion(int value) {
 	
 	glutTimerFunc(10,animacion,1);
     glutPostRedisplay();
 	
 }
+
+
 
 int main (int argc, char** argv) {
 
@@ -239,6 +276,7 @@ int main (int argc, char** argv) {
 	glutCreateWindow("Test Opengl");
 
 	init ();
+	inicializarNots();
 
 	glutReshapeFunc(changeViewport);
 	glutDisplayFunc(render);
---
+++ mainOgre01.cpp
@@ -0,0 +1,104 @@
+#include "Ogre\ExampleApplication.h"
+
+
+
+class Example25FrameListener : public Ogre::FrameListener
+{
+private:
+	Ogre::SceneNode* _node;
+	
+
+public:
+	Ogre::FrameListener* FrameListener01;
+	Example25FrameListener(Ogre::SceneNode* node)
+	{
+		_node = node;
+	}
+
+	bool frameStarted(const Ogre::FrameEvent &evt)
+	{
+		_node->translate(Ogre::Vector3(10,0,0) * evt.timeSinceLastFrame);
+		return true;
+	}
+
+	Example25FrameListener()
+	{
+		FrameListener01 = NULL;
+	}
+	~Example25FrameListener()
+	{
+		if(FrameListener01)
+		{
+			delete FrameListener01;
+		}
+
+	}
+
+};
+
+class Example1 : public ExampleApplication
+{
+private:
+	Ogre::SceneNode* _SinbadNode;
+	
+
+public:	
+	
+	void createCamera() {
+
+		mCamera = mSceneMgr->createCamera("MyCamera1");
+		mCamera->setPosition(0,100,200);
+		mCamera->lookAt(0,0,0);
+		mCamera->setNearClipDistance(5);
+
+	}
+
+	void createFrameListener()
+	{
+		FrameListener*  FL = new Example25FrameListener(_SinbadNode);
+		mRoot->addFrameListener(FL);
+	}
+
+
+	void createScene()
+	{ 
+		Ogre::Plane plane(Vector3::UNIT_Y, -10);
+		Ogre::MeshManager::getSingleton().createPlane("plane",
+			ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plane,
+			1500,1500,200,200,true,1,5,5,Vector3::UNIT_Z);
+		Ogre::Entity* ent = mSceneMgr->createEntity("LightPlaneEntity",
+			"plane");
+		mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent);
+		ent->setMaterialName("Examples/BeachStones");
+
+		Ogre::Light* light = mSceneMgr->createLight("Light1");
+		light->setType(Ogre::Light::LT_DIRECTIONAL);
+		light->setDirection(Ogre::Vector3(1,-1,0));
+
+		Ogre::Light* light2 = mSceneMgr->createLight("Light2");
+		light2->setType(Ogre::Light::LT_DIRECTIONAL);
+		light2->setDirection(Ogre::Vector3(-1,-1,0));
+
+		Ogre::SceneNode* node = mSceneMgr->createSceneNode("Node1");
+		mSceneMgr->getRootSceneNode()->addChild(node);
+		Ogre::Entity* Sinbad = mSceneMgr->createEntity("Sinbad", "Sinbad.mesh");
+			_SinbadNode = node->createChildSceneNode("SinbadNode");
+		_SinbadNode->setScale(3.0f,3.0f,3.0f);
+		_SinbadNode->setPosition(Ogre::Vector3(0.0f,4.0f,0.0f));
+		_SinbadNode->attachObject(Sinbad);
+
+		mSceneMgr->setShadowTechnique(SHADOWTYPE_STENCIL_ADDITIVE);
+	}
+
+};
+
+
+
+
+int main (void)
+{
+
+	Example1 app;
+	app.go();
+	return 0;
+}
