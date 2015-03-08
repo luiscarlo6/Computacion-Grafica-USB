4b572bb5270d0505938a5720b3b51092927eae16
luiscarlo6 <luiscarlo6@gmail.com>
2015-03-07 19:39:31 -0430

    plano

--- Proyecto 7/Ogre_Plantilla_01/Ogre_Plantilla_01/main.cpp
+++ Proyecto 7/Ogre_Plantilla_01/Ogre_Plantilla_01/main.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 
+Ogre::SceneNode* nM03;
+
 class FrameListenerProyectos : public Ogre::FrameListener{
 private:
 	Ogre::SceneNode* nodoF01;
@@ -29,6 +31,7 @@ public:
 			return false;
 
 		Ogre::Vector3 t(0,0,0);
+		Ogre::Vector3 t_ogro(0,0,0);
 
 		if (_key->isKeyDown(OIS::KC_W))
 			t += Ogre::Vector3(0,0,-10);
@@ -42,13 +45,15 @@ public:
 		if (_key->isKeyDown(OIS::KC_D))
 			t += Ogre::Vector3(10,0,0);
 
+
+		t_ogro+= Ogre::Vector3(0,0,-100);
 		_cam->moveRelative(t*evt.timeSinceLastFrame*movSpeed);
 		float rotX = _mouse->getMouseState().X.rel * evt.timeSinceLastFrame* -1;
 		float rotY = _mouse->getMouseState().Y.rel * evt.timeSinceLastFrame * -1;
 		_cam->yaw(Ogre::Radian(rotX));
 		_cam->pitch(Ogre::Radian(rotY));
 		
-
+		//nM03->translate(t_ogro*evt.timeSinceLastFrame);
 		return true;
 	}
 
@@ -136,7 +141,7 @@ public:
 		 _sceneManager =  _root->createSceneManager(Ogre::ST_GENERIC);
 
 		 Ogre::Camera* camera = _sceneManager->createCamera("Camera");
-		 camera->setPosition(Ogre::Vector3(500,100,500));
+		 camera->setPosition(Ogre::Vector3(1100,1100,1100));
 		 camera->lookAt(Ogre::Vector3(0,0,0));
 		 camera->setNearClipDistance(5);
 
@@ -155,35 +160,25 @@ public:
 	 }
 
 	 void createScene(){
-		 _sceneManager->setAmbientLight(Ogre::ColourValue(1.0f,1.0f,1.0f));
-		 Ogre::SceneNode* nodeEsfera02;
-		 Ogre::Light* light02;
+		_sceneManager->setAmbientLight(Ogre::ColourValue(1.0f,1.0f,1.0f));
 
-		 Ogre::SceneNode* nM01 = _sceneManager->createSceneNode("nm01");
-		 Ogre::Entity* entMesh01 = _sceneManager->createEntity("Entnm01", "proyectoOgreI.mesh");
-		 _sceneManager->getRootSceneNode()->addChild(nM01);
-		 nM01->attachObject(entMesh01);
-		 entMesh01->setMaterialName("mat02");
+		Ogre::Plane plane(Ogre::Vector3::UNIT_Y , 0.0);
 
-		 Ogre::SceneNode* nM02 = _sceneManager->createSceneNode("nm02");
-		 Ogre::Entity* entMesh02 = _sceneManager->createEntity("Entnm02", "ejes01.mesh");
-		 _sceneManager->getRootSceneNode()->addChild(nM02);
-		 nM02->attachObject(entMesh02);
-		 nM02->scale(10.0f,10.0f,10.0f);
+		Ogre::MeshManager::getSingleton().createPlane("plane", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, 
+						plane, 10000,20000,200,200,true, 1,10,10, Ogre::Vector3::UNIT_Z);
 
-		 Ogre::SceneNode* nM03 = _sceneManager->createSceneNode("nm03");
-		 Ogre::Entity* entMesh03 = _sceneManager->createEntity("Entnm03", "ogrehead.mesh");
-		 _sceneManager->getRootSceneNode()->addChild(nM03);
-		 nM03->attachObject(entMesh03);
-		 nM03->scale(5.0f,5.0f,5.0f);
-		 entMesh03->setMaterialName("mat01");
+		Ogre::Entity* entPlano = _sceneManager->createEntity("PlanoEntity","plane");
+		_sceneManager->getRootSceneNode()->createChildSceneNode()->attachObject(entPlano);
+		entPlano->setMaterialName("mat02");
 
-		 Ogre::Entity* entEsfera02 = _sceneManager->createEntity("EntEsfera02","sphere.mesh");
+		Ogre::SceneNode* nodeEsfera02;
+		Ogre::Light* light02;
+		Ogre::Entity* entEsfera02 = _sceneManager->createEntity("EntEsfera02","sphere.mesh");
 		//Ogre::SceneNode* nodeEsfera02 = mSceneMgr->createSceneNode("NodeEsfera02");
 		nodeEsfera02 = _sceneManager->createSceneNode("NodeEsfera02");
 		_sceneManager->getRootSceneNode()->addChild(nodeEsfera02);
 		nodeEsfera02->attachObject(entEsfera02);
-
+		
 		 //NODO LUZ
 		float lightScale = 0.9f;
 		Ogre::SceneNode* nodeLuz02 = _sceneManager->createSceneNode("NodeLuz02");		
@@ -195,8 +190,14 @@ public:
 
 		nodeEsfera02->addChild(nodeLuz02);
 		nodeEsfera02->setScale(0.05f,0.05f,0.05f);
-		nodeEsfera02->setPosition(-500.0f,500.0f,500.0f);
+		nodeEsfera02->setPosition(0.0f,5000.0f,0.0f);
 
+
+		Ogre::Entity* ent01 = _sceneManager->createEntity("MyEntity1","ejes01.mesh");
+		Ogre::SceneNode* node01 = _sceneManager->createSceneNode("Node01");
+		_sceneManager->getRootSceneNode()->addChild(node01);
+		node01->attachObject(ent01);
+		node01->setScale(100.0f,100.0f,100.0f);
 	 }
 };
 
